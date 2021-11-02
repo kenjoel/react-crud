e44d2d426a565e3caa20d16c89b74ce099246518
kenconan <kenconan@ken>
2021-11-02 03:48:17 -0400

    routes

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
-import Nav from "./components/Nav"
+import Menu from './components/Menu';
+import Nav from "./components/Nav";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Products from './admin/Products';
+BrowserRouter
 
 function App() {
   return (
@@ -8,47 +12,16 @@ function App() {
       <Nav />
       <body>
     
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-      <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"/>
-      <div className="navbar-nav">
-        <div className="nav-item text-nowrap">
-          <a className="nav-link px-3" href="#">Sign out</a>
-        </div>
-      </div>
-    </header>
-    
     <div className="container-fluid">
       <div className="row">
+        <Menu/>
         <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-    
-    
-          <h2>Section title</h2>
-          <div className="table-responsive">
-            <table className="table table-striped table-sm">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <BrowserRouter>
+          
+          <Route path="admin/products" component={Products}/>
+
+          </BrowserRouter>
+          
         </main>
       </div>
     </div>
